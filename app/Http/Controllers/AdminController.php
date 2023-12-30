a1bebdab7255ab196cb56a6cc3a14d9e7316bbe9
Arefin21 <arefinshagor18@gmail.com>
2023-12-30 11:45:47 +0600

    loginLogout Toast

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -15,7 +15,12 @@ class AdminController extends Controller {
 
         $request->session()->regenerateToken();
 
-        return redirect('/login');
+        $notification = [
+            'message'    => 'Admin Logout Successfully',
+            'alert-type' => 'success',
+        ];
+
+        return redirect('/login')->with($notification);
     }
 
     public function Profile() {
